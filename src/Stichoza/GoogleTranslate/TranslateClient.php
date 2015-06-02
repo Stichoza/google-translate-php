b4941390d9ab2e6c48b4706416cd613bc2095bad
Levan Velijanashvili <stichoza@gmail.com>
2015-06-02 18:23:00 +0400

    Fix default value of detected source

--- src/Stichoza/GoogleTranslate/TranslateClient.php
+++ src/Stichoza/GoogleTranslate/TranslateClient.php
@@ -270,7 +270,7 @@ class TranslateClient
         }
 
         // Set initial detected language to null
-        $this::$lastDetectedSource = null;
+        $this::$lastDetectedSource = false;
 
         // Iterate and set last detected language
         foreach ($detectedLanguages as $lang) {
