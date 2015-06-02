74eb36ba2605e1b85dabe2ddbc259a7f88b76c5c
Levan Velijanashvili <stichoza@gmail.com>
2015-06-02 18:00:17 +0400

    Test testStaticAndNonstaticDetection

--- tests/LanguageDetectionTest.php
+++ tests/LanguageDetectionTest.php
@@ -36,4 +36,24 @@ class LanguageDetectionTest extends \PHPUnit_Framework_TestCase
         $this->tr->translate('Ще не вмерла Україна, И слава, и воля! Ще намъ, браття-молодці, Усміхнеться доля!');
         $this->assertEquals($this->tr->getLastDetectedSource(), 'uk');
     }
+
+    public function testStaticAndNonstaticDetection()
+    {
+        $this->tr->translate('გამარჯობა');
+        
+        TranslateClient::translate('Cześć');
+
+        $this->assertNotEquals($this->tr->getLastDetectedSource(), 'ka');
+        $this->assertNotEquals(TranslateClient::getLastDetectedSource(), 'ka');
+
+        $this->assertEquals($this->tr->getLastDetectedSource(), 'uk');
+        $this->assertEquals(TranslateClient::getLastDetectedSource(), 'uk');
+
+        $this->tr->translate('გამარჯობა');
+
+        $this->assertNotEquals($this->tr->getLastDetectedSource(), 'uk');
+        $this->assertNotEquals(TranslateClient::getLastDetectedSource(), 'uk');
+        $this->assertEquals($this->tr->getLastDetectedSource(), 'ka');
+        $this->assertEquals(TranslateClient::getLastDetectedSource(), 'ka');
+    }
 }
