e28248893b44a534a822adce794005727041ae33
Levan Velijanashvili <stichoza@gmail.com>
2015-12-21 17:52:50 +0400

    Token provider interface and sample implementation

---
+++ src/Stichoza/GoogleTranslate/Tokens/SampleTokenGenerator.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Stichoza\GoogleTranslate\Tokens;
+
+/**
+ * A nice interface for providing tokens.
+ */
+class SampleTokenGenerator implements TokenProviderInterface
+{
+	/**
+	 * Generate a fake token just as an example
+	 */
+	public function generateToken($source, $target, $text) {
+		return sprintf('%d.%d', rand(10000, 99999), rand(10000, 99999));
+	}
+}
---
+++ src/Stichoza/GoogleTranslate/Tokens/TokenProviderInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Stichoza\GoogleTranslate\Tokens;
+
+/**
+ * A nice interface for providing tokens.
+ */
+interface TokenProviderInterface
+{
+	/**
+	 * Generate and return a token
+	 * 
+	 * @param string $source Source language
+	 * @param string $target Target language
+	 * @param string $text Text to translate
+	 * @return mixed A token
+	 */
+	public function generateToken($source, $target, $text);
+}
