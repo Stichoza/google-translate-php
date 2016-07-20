d007f8796f1d79a7c42eaecbc0f525ce00414cf9
consatan <consatan@gmail.com>
2016-07-20 14:12:18 +0800

    Add setHttpOption test case

--- tests/UtilityTest.php
+++ tests/UtilityTest.php
@@ -35,4 +35,30 @@ class UtilityTest extends \PHPUnit_Framework_TestCase
             $this->assertEquals($m->invokeArgs($t, [$key]), $value);
         }
     }
+
+    public function testSetHttpOption()
+    {
+        $res = fopen('php://memory', 'r+');
+
+        $this->tr->setHttpOption([
+            'debug'   => $res,
+            'headers' => [
+                'User-Agent' => 'Foo',
+            ],
+        ])->translate('hello');
+        rewind($res);
+        $output = str_replace("\r", '', stream_get_contents($res));
+        $this->assertContains('User-Agent: Foo', $output);
+
+        $this->tr->setHttpOption([
+            'debug'   => $res,
+            'headers' => [
+                'User-Agent' => 'Bar',
+            ],
+        ])->translate('world');
+        rewind($res);
+        $output = str_replace("\r", '', stream_get_contents($res));
+        $this->assertContains('User-Agent: Bar', $output);
+        fclose($res);
+    }
 }
