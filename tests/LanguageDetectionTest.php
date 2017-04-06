879992f26d6f55a3bac6b0e4ac08bfebd3ad6263
Anass BENOMAR <abenomar@umanis.com>
2017-04-06 23:19:42 +0100

    method PSR-1

--- tests/LanguageDetectionTest.php
+++ tests/LanguageDetectionTest.php
@@ -38,7 +38,7 @@ class LanguageDetectionTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($this->tr->getLastDetectedSource(), 'uk');
     }
 
-    public function testStaticAndNonstaticDetection()
+    public function testStaticAndNonStaticDetection()
     {
         $this->tr->translate('გამარჯობა');
 
