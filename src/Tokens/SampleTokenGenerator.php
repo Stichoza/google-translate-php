c5094b4c9e94918a7038b3498706ff8921ab883e
Levan Velijanashvili <stichoza@gmail.com>
2022-11-29 03:04:41 +0400

    Simple token example

--- src/Tokens/SampleTokenGenerator.php
+++ src/Tokens/SampleTokenGenerator.php
@@ -17,6 +17,6 @@ class SampleTokenGenerator implements TokenProviderInterface
      */
     public function generateToken(string $source, string $target, string $text): string
     {
-        return sprintf('%d.%d', rand(10000, 99999), rand(10000, 99999));
+        return microtime(true);
     }
 }
