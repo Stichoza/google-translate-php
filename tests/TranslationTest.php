8720059c1f216b17c2befa6771998ac16c0c7052
Levan Velijanashvili <stichoza@gmail.com>
2022-11-28 23:11:01 +0400

    Add test for large text translation

--- tests/TranslationTest.php
+++ tests/TranslationTest.php
@@ -38,6 +38,16 @@ class TranslationTest extends TestCase
         $this->assertEquals($resultOne, $resultTwo, 'ja 👍🏽');
     }
 
+    public function testLargeTextTranslation()
+    {
+        $text = "Google Translate is a multilingual neural machine translation service developed by Google to translate text, documents and websites from one language into another. It offers a website interface, a mobile app for Android and iOS, and an API that helps developers build browser extensions and software applications. As of November 2022, Google Translate supports 133 languages at various levels, and as of April 2016, claimed over 500 million total users, with more than 100 billion words translated daily, after the company stated in May 2013 that it served over 200 million people daily.";
+
+        $output = $this->tr->setTarget('uk')->translate($text);
+
+        $this->assertIsString($output);
+        $this->assertNotEquals($text, $output);
+    }
+
     public function testRawResponse()
     {
         $rawResult = $this->tr->getResponse('cat');
