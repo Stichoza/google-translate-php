5776f50750a0187bf3f287ec1935865ec97129bc
Levan Velijanashvili <stichoza@gmail.com>
2022-11-30 01:22:12 +0400

    Create tests for all types exceptions

---
+++ tests/ExceptionTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Stichoza\GoogleTranslate\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Stichoza\GoogleTranslate\Exceptions\LargeTextException;
+use Stichoza\GoogleTranslate\Exceptions\RateLimitException;
+use Stichoza\GoogleTranslate\Exceptions\TranslationDecodingException;
+use Stichoza\GoogleTranslate\Exceptions\TranslationRequestException;
+use Stichoza\GoogleTranslate\GoogleTranslate;
+
+class ExceptionTest extends TestCase
+{
+    public GoogleTranslate $tr;
+
+    public function setUp(): void
+    {
+        $this->tr = new GoogleTranslate();
+    }
+
+    public function testRateLimitException(): void
+    {
+        $this->expectException(RateLimitException::class);
+
+        $this->tr->setUrl('https://httpstat.us/429')->translate('Test');
+    }
+
+    public function testRateLimitCaptchaException(): void
+    {
+        $this->expectException(RateLimitException::class);
+
+        $this->tr->setUrl('https://httpstat.us/503')->translate('Test');
+    }
+
+    public function testLargeTextException(): void
+    {
+        $this->expectException(LargeTextException::class);
+
+        $this->tr->setUrl('https://httpstat.us/413')->translate('Test');
+    }
+
+    public function testTranslationRequestException(): void
+    {
+        $this->expectException(TranslationRequestException::class);
+
+        $this->tr->setUrl('https://httpstat.us/418')->translate('Test');
+    }
+
+    public function testTranslationDecodingException(): void
+    {
+        $this->expectException(TranslationDecodingException::class);
+
+        $this->tr->setUrl('https://httpstat.us/200')->translate('Test');
+    }
+}
