d20f4506b59e257a2c9c83f4f217666b38f41d5c
Levan Velijanashvili <stichoza@gmail.com>
2022-11-30 03:16:36 +0400

    Add messages

--- tests/LanguageDetectionTest.php
+++ tests/LanguageDetectionTest.php
@@ -17,27 +17,27 @@ class LanguageDetectionTest extends TestCase
     public function testSingleWord(): void
     {
         $this->tr->translate('გამარჯობა');
-        $this->assertEquals($this->tr->getLastDetectedSource(), 'ka');
+        $this->assertEquals($this->tr->getLastDetectedSource(), 'ka', 'Language detection should work for single words');
 
         $this->tr->translate('Cześć');
-        $this->assertEquals($this->tr->getLastDetectedSource(), 'pl');
+        $this->assertEquals($this->tr->getLastDetectedSource(), 'pl', 'Language detection should work for single words');
     }
 
     public function testSingleSentence(): void
     {
         $this->tr->translate('იყო არაბეთს როსტევან');
-        $this->assertEquals($this->tr->getLastDetectedSource(), 'ka');
+        $this->assertEquals($this->tr->getLastDetectedSource(), 'ka', 'Language detection should work for sentences');
 
         $this->tr->translate('Путин хуйло');
-        $this->assertEquals($this->tr->getLastDetectedSource(), 'ru');
+        $this->assertEquals($this->tr->getLastDetectedSource(), 'ru', 'Language detection should work for multiple sentences');
     }
 
     public function testMultipleSentence(): void
     {
         $this->tr->translate('ჩემი ხატია სამშობლო. სახატე - მთელი ქვეყანა. განათებული მთა-ბარი.');
-        $this->assertEquals($this->tr->getLastDetectedSource(), 'ka');
+        $this->assertEquals($this->tr->getLastDetectedSource(), 'ka', 'Language detection should work for multiple sentences');
 
         $this->tr->translate('Ще не вмерла Україна, И слава, и воля! Ще намъ, браття-молодці, Усміхнеться доля!');
-        $this->assertEquals($this->tr->getLastDetectedSource(), 'uk');
+        $this->assertEquals($this->tr->getLastDetectedSource(), 'uk', 'Language detection should work for multiple sentences');
     }
 }
